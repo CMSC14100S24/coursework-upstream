1dd9d32d179d577e13851f860a17c10f72a522e5
BrennanSchaffner <49691845+BrennanSchaffner@users.noreply.github.com>
2024-06-04 18:45:20 -0400

    Added materials for HW1

---
+++ hw1/hw1.py
@@ -0,0 +1,24 @@
+"""
+CMSC 14100
+Summer 2024
+Homework #1
+
+We will be using anonymous grading, so please do NOT include your name
+in this file
+
+People Consulted:
+   For future assignments, you will be expected to list anyone (other than the
+   course staff that you consulted about this assignment).
+
+Online resources consulted:
+   For future assignments, you will be expected to list the URL of
+   any online resources that you use to complete this assignment.
+"""
+
+def add1and1():
+   # Replace None with 1 + 1 in the next line.
+    return None
+
+if __name__ == "__main__":
+    print("1 + 1 is", add1and1())
+    
